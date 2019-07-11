98c251ad7a9700b70f4c30f774dec44385025b1f
Thinh Tran <quangthinhtran3588@gmail.com>
2019-07-11 13:46:33 +0700

    add linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,26 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    'plugin:@typescript-eslint/recommended', // uses typescript-specific linting rules
+    'plugin:react/recommended', // uses react-specific linting rules
+    'plugin:prettier/recommended', // enables eslint-plugin-prettier and eslint-config-prettier
+    'prettier/react', // disables react-specific linting rules that conflict with prettier
+    '@react-native-community',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,  // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module',  // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true,  // Allows for the parsing of JSX
+    },
+  },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  },
+  settings: {
+    react: {
+      version: 'detect',  // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
 };
