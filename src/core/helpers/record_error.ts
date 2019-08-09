54c04084ab7bd138b46c6fba9d4c1c1fb988f063
Thinh Tran <quangthinhtran3588@gmail.com>
2019-08-09 17:26:42 +0700

    add additional info in recording error

--- src/core/helpers/record_error.ts
+++ src/core/helpers/record_error.ts
@@ -3,14 +3,18 @@ import crashlytics from '@react-native-firebase/crashlytics';
 import auth from '@react-native-firebase/auth';
 import { config } from '@app/config';
 
-export const recordError = (error: Error): void => {
-  if (__DEV__) {
+export const recordError = async (error: Error): Promise<void> => {
+  if (!__DEV__) {
     try {
       const { currentUser } = auth();
-      if (currentUser) {
-        crashlytics().log(`userId: ${currentUser.uid}`);
-      }
-      crashlytics().log(`appVersion: ${Platform.OS === 'ios' ? config.ios.version : config.android.version}`);
+      await Promise.all([
+        crashlytics().setUserId(currentUser ? currentUser.uid : ''),
+        crashlytics().setUserName(currentUser ? currentUser.displayName : ''),
+        crashlytics().setAttribute(
+          'appVersion',
+          `${Platform.OS === 'ios' ? config.ios.version : config.android.version}`,
+        ),
+      ]);
       crashlytics().recordError(error);
     } catch (internalError) {
       // do nothing
